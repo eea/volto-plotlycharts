e6d8962c0587eb6d4bc9363c980163170850ebe1
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-11-20 23:22:14 +0200

    Added embed chart block

--- src/config.js
+++ src/config.js
@@ -1,12 +1,24 @@
 import ChartWidget from './Widget/Widget';
 import VisualizationView from './Visualization/View';
 
-import PlotlyBlockEdit from './Block/Edit';
-import PlotlyBlockView from './Block/View';
+import PlotlyBlockEdit from './ChartBlock/Edit';
+import PlotlyBlockView from './ChartBlock/View';
+
+import EmbedChartBlockEdit from './ChartBlock/Edit';
+import EmbedChartBlockView from './ChartBlock/View';
 
 import chartIcon from '@plone/volto/icons/world.svg';
 
 export function applyConfig(config) {
+  const hasCustomGroup = config.blocks.groupBlocksOrder.filter(
+    el => el.id === 'custom_addons',
+  );
+  if (!hasCustomGroup) {
+    config.blocks.groupBlocksOrder.push({
+      id: 'custom_addons',
+      title: 'Custom addons',
+    });
+  }
   config.contentTypeViews.visualization = VisualizationView;
   config.widgets.id.viualization = ChartWidget;
   config.blocks.blocksConfig.plotly_charts = {
@@ -14,6 +26,14 @@ export function applyConfig(config) {
     title: 'Plotly Chart',
     view: PlotlyBlockView,
     edit: PlotlyBlockEdit,
+    icon: chartIcon,
+    group: 'custom_addons',
+  };
+  config.blocks.blocksConfig.embed_chart = {
+    id: 'embed_chart',
+    title: 'Embed Chart',
+    view: EmbedChartBlockView,
+    edit: EmbedChartBlockEdit,
     icon: chartIcon,
     group: 'custom_addons',
   };
