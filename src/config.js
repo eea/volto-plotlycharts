8480d65ca7bee1c9fe27bfed4a50cae272748685
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-11-20 22:54:19 +0200

    Added block chart

--- src/config.js
+++ src/config.js
@@ -1,19 +1,22 @@
-import ChartWidget from './PlotlyChart/Widget';
+import ChartWidget from './Widget/Widget';
 import VisualizationView from './Visualization/View';
 
+import PlotlyBlockEdit from './Block/Edit';
+import PlotlyBlockView from './Block/View';
+
+import chartIcon from '@plone/volto/icons/world.svg';
+
 export function applyConfig(config) {
-  return {
-    ...config,
-    contentTypeViews: {
-      ...config.contentTypeViews,
-      visualization: VisualizationView,
-    },
-    widgets: {
-      ...config.widgets,
-      id: {
-        ...config.widgets.id,
-        visualization: ChartWidget,
-      },
-    },
+  config.contentTypeViews.visualization = VisualizationView;
+  config.widgets.id.viualization = ChartWidget;
+  config.blocks.blocksConfig.plotly_charts = {
+    id: 'plotly_chart',
+    title: 'Plotly Chart',
+    view: PlotlyBlockView,
+    edit: PlotlyBlockEdit,
+    icon: chartIcon,
+    group: 'custom_addons',
   };
+
+  return config;
 }
