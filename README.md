2ed6690a23732e23115d6bc0b65753303c3018e3
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-11-20 22:41:44 +0200

    Added visualization view

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # volto-plotlycharts
 
 Plotly Charts and Plotly Chart Editor integration with Volto
+
+- Exports a PlotlyChart Widget, can be used in regular volto edit forms
+- Registers a VisualizationView component for a content type named
+  'visualization'. `eea.restapi` has a behavior to implement such a content
+  type.
--- src/config.js
+++ src/config.js
@@ -1,8 +1,13 @@
 import ChartWidget from './PlotlyChart/Widget';
+import VisualizationView from './Visualization/View';
 
 export function applyConfig(config) {
   return {
     ...config,
+    contentTypeViews: {
+      ...config.contentTypeViews,
+      visualization: VisualizationView,
+    },
     widgets: {
       ...config.widgets,
       id: {
