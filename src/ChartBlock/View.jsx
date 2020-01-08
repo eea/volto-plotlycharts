b89c22b4c5550742b7540647d00b57f5f93269a2
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-01-08 09:29:19 +0200

    Refresh chart data with provider data, on chart display

--- src/ChartBlock/View.jsx
+++ src/ChartBlock/View.jsx
@@ -1,6 +1,8 @@
+import { addAppURL } from '@plone/volto/helpers';
+import { getDataFromProvider } from 'volto-datablocks/actions';
+import { connect } from 'react-redux';
 import Loadable from 'react-loadable';
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect } from 'react';
 
 const LoadablePlot = Loadable({
   loader: () => import('react-plotly.js'),
@@ -9,31 +11,82 @@ const LoadablePlot = Loadable({
   },
 });
 
-class ChartView extends Component {
-  constructor(props) {
-    super(props);
-    // console.log('ChartView props', props.data.chartData);
-    this.state = { ...props.data.chartData };
-  }
+function mixProviderData(chartData, providerData) {
+  const providerDataColumns = Object.keys(providerData);
 
-  render() {
-    return (
-      <div className="chartWrapperView">
-        <div className="block-inner-wrapper">
-          <LoadablePlot
-            data={this.state.data}
-            layout={this.state.layout}
-            frames={this.state.frames}
-            config={{ displayModeBar: false }}
-          />
-        </div>
-      </div>
-    );
-  }
+  const res = chartData.map(trace => {
+    Object.keys(trace).forEach(tk => {
+      const originalColumn = tk.replace(/src$/, '');
+      if (
+        tk.endsWith('src') &&
+        Object.keys(trace).includes(originalColumn) &&
+        typeof trace[tk] === 'string' &&
+        providerDataColumns.includes(trace[tk])
+      ) {
+        // console.log('replacing', originalColumn, tk, trace[tk]);
+        let values = providerData[trace[tk]];
+
+        // if (originalColumn === 'labels') values = values.map(l => l + 'XXX');
+
+        trace[originalColumn] = values;
+      }
+    });
+
+    return trace;
+  });
+  // console.log('new chartData', res);
+  return res;
 }
 
-ChartView.propTypes = {
-  data: PropTypes.objectOf(PropTypes.any).isRequired,
-};
+function ChartView(props) {
+  // console.log(props);
 
-export default ChartView;
+  useEffect(() => {
+    props.getDataFromProvider(props.data.url);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.data.url]);
+
+  const chartData = props.data.chartData;
+  return (
+    <div className="chartWrapperView">
+      <div className="block-inner-wrapper">
+        <LoadablePlot
+          data={
+            props.providerData
+              ? mixProviderData(chartData.data, props.providerData)
+              : chartData.data
+          }
+          layout={chartData.layout}
+          frames={chartData.frames}
+          config={{ displayModeBar: false }}
+        />
+      </div>
+    </div>
+  );
+}
+
+function getProviderData(state, props) {
+  let path = props?.data?.url || null;
+  // console.log('url in getProviderData', path, state);
+
+  if (!path) return;
+
+  path = `${path}/@connector-data`;
+  const url = `${addAppURL(path)}/@connector-data`;
+
+  const data = state.data_providers.data || {};
+  const res = path ? data[path] || data[url] : [];
+  // console.log('res', res);
+  return res;
+}
+
+export default connect(
+  (state, props) => {
+    const providerData = getProviderData(state, props);
+
+    return {
+      providerData,
+    };
+  },
+  { getDataFromProvider },
+)(ChartView);
