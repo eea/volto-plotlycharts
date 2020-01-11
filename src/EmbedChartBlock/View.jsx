cbb04424ef5c52ce6345322ee3b536801d1c0e5c
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-01-11 17:02:41 +0200

    Simplify embedded chart block

--- src/EmbedChartBlock/View.jsx
+++ src/EmbedChartBlock/View.jsx
@@ -4,29 +4,6 @@ import { Grid } from 'semantic-ui-react';
 import ConnectedChart from '../ConnectedChart';
 
 class EmbedChartView extends Component {
-  constructor(props) {
-    super(props);
-
-    // console.log('chart data', this.props.data, this.props);
-    // TODO: there's no need for state and these checks, get rid of them
-    let chartData = this.props.data.chartData || [];
-
-    if (typeof chartData === 'string') {
-      try {
-        chartData = JSON.parse(chartData);
-      } catch (error) {
-        console.log('Error in JSON parsing chart data', error);
-        chartData = {};
-      }
-    }
-
-    // TODO: the axis labels need to come from the data
-
-    this.state = {
-      chartData,
-    };
-  }
-
   render() {
     let text = this.props.data['chart-text'];
     if (typeof text !== 'string') text = '';
@@ -40,9 +17,9 @@ class EmbedChartView extends Component {
               </div>
             </Grid.Column>
             <Grid.Column width={8}>
-              {this.state.chartData ? (
+              {this.props.data.chartData ? (
                 <ConnectedChart
-                  data={this.state.chartData}
+                  data={this.props.data.chartData}
                   className="embedded-block-chart"
                 />
               ) : (
