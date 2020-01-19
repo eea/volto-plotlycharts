4d01b82508e857ba98eb32b3b0c8dd74a13efe26
Mihai Macaneata <mihai.macaneata@eaudeweb.ro>
2020-01-19 16:49:49 +0200

    source view

--- src/ChartBlock/View.jsx
+++ src/ChartBlock/View.jsx
@@ -6,6 +6,11 @@ const ChartView = props => {
     <div className="chartWrapperView">
       <div className="block-inner-wrapper">
         <ConnectedChart {...props} className="chart-block-chart" />
+        <div>
+          <a className="discreet" href={props.data.chart_source_link}>
+            {props.data.chart_source}
+          </a>
+        </div>
       </div>
     </div>
   );
