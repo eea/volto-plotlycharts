19cde21e245d6f4cd70692dbeb6e1d7275c61638
Mihai Macaneata <mihai.macaneata@eaudeweb.ro>
2020-01-19 14:52:22 +0200

    fix chart grid

--- src/EmbedChartBlock/View.jsx
+++ src/EmbedChartBlock/View.jsx
@@ -10,29 +10,34 @@ const EmbedChartView = props => {
       {props.data.block_title ? <h5>{props.data.block_title}</h5> : ''}
       <div className="block-inner-wrapper">
         <Grid>
-          <Grid.Column width={4}>
-            <div className="block-text-content" style={{ marginTop: '2rem' }}>
-              <ViewText {...props} />
-            </div>
-          </Grid.Column>
-          <Grid.Column width={8}>
-            {props.data.chartData ? (
-              <ConnectedChart
-                source={props.data.vis_url}
-                data={props.data.chartData}
-                className="embedded-block-chart"
-              />
-            ) : (
-              <div>No valid data.</div>
-            )}
-          </Grid.Column>
-          <Grid.Column width={12}>
-            <div>
-              <a className="discreet" href={props.data.chart_source_link}>
-                {props.data.chart_source}
-              </a>
-            </div>
-          </Grid.Column>
+          <Grid.Row>
+            <Grid.Column width={4}>
+              <div
+                className="block-text-content"
+                style={{ padding: '1rem', marginTop: '.5rem' }}
+              >
+                <ViewText {...props} />
+              </div>
+            </Grid.Column>
+            <Grid.Column width={8}>
+              {props.data.chartData ? (
+                <ConnectedChart
+                  source={props.data.vis_url}
+                  data={props.data.chartData}
+                  className="embedded-block-chart"
+                />
+              ) : (
+                <div>No valid data.</div>
+              )}
+            </Grid.Column>
+            <Grid.Column width={12}>
+              <div>
+                <a className="discreet" href={props.data.chart_source_link}>
+                  {props.data.chart_source}
+                </a>
+              </div>
+            </Grid.Column>
+          </Grid.Row>
         </Grid>
       </div>
     </div>
