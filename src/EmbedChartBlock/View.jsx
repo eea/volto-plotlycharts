5922a049270bebb588b391ba9b2e46191fcb7205
Mihai Macaneata <mihai.macaneata@eaudeweb.ro>
2020-01-30 12:45:39 +0200

    element grid on embed chart block

--- src/EmbedChartBlock/View.jsx
+++ src/EmbedChartBlock/View.jsx
@@ -6,56 +6,75 @@ import ViewText from '@plone/volto/components/manage/Blocks/Text/View';
 import SourceView from 'volto-datablocks/theme/Blocks/SourceView';
 import { connect } from 'react-redux';
 
+import WidthBasedLayoutProvider from '~/components/theme/LayoutProvider/WidthBasedLayoutProvider';
+
 const EmbedChartView = props => {
   const hasText =
     (props.data.text?.blocks?.length > 1 && props.data.text?.blocks) ||
     (props.data.text?.blocks?.length === 1 &&
       props.data.text?.blocks?.[0].text);
+
+  const grid = {
+    text_column: {
+      phone: 'twelve',
+      tablet: 'twelve',
+      desktop: 'four',
+      widescreen: 'four',
+    },
+    chart_column: {
+      phone: 'twelve',
+      tablet: 'twelve',
+      desktop: hasText ? 'eight' : 'twelve',
+      widescreen: hasText ? 'eight' : 'twelve',
+    },
+  };
+
   // console.log('embed chart props', props);
   return (
     <div className="chartWrapperView">
       {props.data.block_title ? <h5>{props.data.block_title}</h5> : ''}
       <div className="block-inner-wrapper">
-        <Grid>
-          <Grid.Row>
-            {hasText ? (
-              <Grid.Column computer={4} largeScreen={4} tablet={12} mobile={12}>
-                <div
-                  className="block-text-content"
-                  style={{ padding: '1rem', marginTop: '.5rem' }}
-                >
-                  <ViewText {...props} />
-                </div>
-              </Grid.Column>
-            ) : (
-              ''
-            )}
-            <Grid.Column
-              computer={hasText ? 8 : 12}
-              largeScreen={hasText ? 8 : 12}
-              tablet={12}
-              mobile={12}
+        <div className="element-grid">
+          {hasText ? (
+            <div
+              className={`${props.layout_type}-${
+                grid.text_column[props.layout_type]
+              }`}
             >
-              {props.data.chartData ? (
-                <ConnectedChart
-                  source={props.data.vis_url}
-                  data={props.data.chartData}
-                  className="embedded-block-chart"
-                />
-              ) : (
-                <div>No valid data.</div>
-              )}
-            </Grid.Column>
-            <div>
-              <SourceView
-                initialSource={props.data.chart_source}
-                initialSourceLink={props.data.chart_source_link}
-                multipleSources={props.data.chartSources}
-                providerUrl={props.providerUrl}
-              />
+              <div
+                className="block-text-content"
+                style={{ padding: '1rem', marginTop: '.5rem' }}
+              >
+                <ViewText {...props} />
+              </div>
             </div>
-          </Grid.Row>
-        </Grid>
+          ) : (
+            ''
+          )}
+          <div
+            className={`${props.layout_type}-${
+              grid.chart_column[props.layout_type]
+            }`}
+          >
+            {props.data.chartData ? (
+              <ConnectedChart
+                source={props.data.vis_url}
+                data={props.data.chartData}
+                className="embedded-block-chart"
+              />
+            ) : (
+              <div>No valid data.</div>
+            )}
+          </div>
+          <div>
+            <SourceView
+              initialSource={props.data.chart_source}
+              initialSourceLink={props.data.chart_source_link}
+              multipleSources={props.data.chartSources}
+              providerUrl={props.providerUrl}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -76,4 +95,4 @@ export default connect(
     };
   },
   {},
-)(EmbedChartView);
+)(WidthBasedLayoutProvider(EmbedChartView));
