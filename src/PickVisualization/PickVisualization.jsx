96936050e82a1bedb6f2f699a2dde2582b401f37
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-01-31 23:43:09 +0200

    Code cleanup

--- src/PickVisualization/PickVisualization.jsx
+++ src/PickVisualization/PickVisualization.jsx
@@ -8,20 +8,7 @@ import React, { Component } from 'react';
 import PickObject from 'volto-addons/PickObject';
 
 class PickVisualization extends Component {
-  // searchVisualizations = () => {
-  //   this.props.searchContent(
-  //     '',
-  //     {
-  //       // object_provides: 'forests.content.interfaces.IDataVisualization',
-  //       portal_type: 'visualization',
-  //     },
-  //     this.props.id,
-  //   );
-  // };
-
   componentDidMount() {
-    // this.searchVisualizations();
-
     if (this.props.value) {
       this.props.getContent(this.props.value, null, this.props.value);
     }
@@ -53,16 +40,27 @@ class PickVisualization extends Component {
 
 export default connect(
   (state, props) => {
-    // let visualizations = state.search
-    //   ? state.search.subrequests?.[props.id]?.items || []
-    //   : [];
-    // visualizations = visualizations.map(el => [el['@id'], el.title]);
-
     const content = state.content.subrequests?.[props.value] || {};
     return {
       remoteChartData: content.data?.visualization || {},
-      // visualizations,
     };
   },
   { searchContent, getContent },
 )(PickVisualization);
+
+// visualizations,
+// this.searchVisualizations();
+// let visualizations = state.search
+//   ? state.search.subrequests?.[props.id]?.items || []
+//   : [];
+// visualizations = visualizations.map(el => [el['@id'], el.title]);
+// searchVisualizations = () => {
+//   this.props.searchContent(
+//     '',
+//     {
+//       // object_provides: 'forests.content.interfaces.IDataVisualization',
+//       portal_type: 'visualization',
+//     },
+//     this.props.id,
+//   );
+// };
