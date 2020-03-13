533e2ec025069dfa61d39198a1ccb380c7de3cc1
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-03-13 21:34:19 +0200

    Rework import model

--- src/Widget/ChartEditor.jsx
+++ src/Widget/ChartEditor.jsx
@@ -27,6 +27,56 @@ function getDataSourceOptions(data) {
   }));
 }
 
+const chartHelp = {
+  area: {
+    helpDoc: 'https://help.plot.ly/make-an-area-graph/',
+    examplePlot: () => {
+      console.log('example bar plot!');
+    },
+  },
+  bar: {
+    helpDoc: 'https://help.plot.ly/stacked-bar-chart/',
+    examplePlot: () => {
+      console.log('example bar plot!');
+    },
+  },
+  box: { helpDoc: 'https://help.plot.ly/make-a-box-plot/' },
+  candlestick: { helpDoc: 'https://help.plot.ly/make-a-candlestick/' },
+  choropleth: { helpDoc: 'https://help.plot.ly/make-a-choropleth-map/' },
+  contour: { helpDoc: 'https://help.plot.ly/make-a-contour-plot/' },
+  heatmap: { helpDoc: 'https://help.plot.ly/make-a-heatmap/' },
+  histogram2d: { helpDoc: 'https://help.plot.ly/make-a-2d-histogram-heatmap/' },
+  histogram2dcontour: { helpDoc: 'https://help.plot.ly/make-a-histogram/' },
+  line: { helpDoc: 'https://help.plot.ly/make-a-line-graph/' },
+  mesh3d: { helpDoc: null },
+  ohlc: { helpDoc: 'https://help.plot.ly/make-a-ohlc/' },
+  pie: { helpDoc: 'https://help.plot.ly/make-a-pie-chart/' },
+  scatter3d: { helpDoc: 'https://help.plot.ly/make-a-3d-scatter-plot/' },
+  line3d: { helpDoc: null },
+  scatter: { helpDoc: 'https://help.plot.ly/how-to-make-a-scatter-plot/' },
+  scattergeo: { helpDoc: 'https://help.plot.ly/make-scatter-map/' },
+  scattermapbox: { helpDoc: 'https://help.plot.ly/make-a-mapbox-map/' },
+  scatterternary: { helpDoc: 'https://help.plot.ly/ternary-scatter-plot/' },
+  surface: { helpDoc: 'https://help.plot.ly/make-a-3d-surface-plot/' },
+  table: { helpDoc: null },
+  timeseries: { helpDoc: 'https://help.plot.ly/range-slider/' },
+};
+
+const imports = {
+  PlotlyEditor: import(
+    /* webpackChunkName: 'plotlyeditor' */ 'react-chart-editor'
+  ),
+  plotly: import(/* webpackChunkName: 'plotlydist' */ 'plotly.js/dist/plotly'),
+};
+
+const resolveImports = async imports => {
+  const res = {};
+  for (const name in imports) {
+    await imports[name].then(module => (res[name] = module.default));
+  }
+  return res;
+};
+
 class Edit extends Component {
   constructor(props) {
     super(props);
@@ -35,15 +85,9 @@ class Edit extends Component {
       PlotlyEditor: null,
     };
   }
-  componentDidMount() {
-    import(/* webpackChunkName: 'plotlyeditor' */ 'react-chart-editor').then(
-      module =>
-        this.setState({ PlotlyEditor: module.default }, () =>
-          import(
-            /* webpackChunkName: 'plotlydist' */ 'plotly.js/dist/plotly'
-          ).then(module => this.setState({ plotly: module.default })),
-        ),
-    );
+  async componentDidMount() {
+    const modules = await resolveImports(imports);
+    this.setState({ ...modules });
   }
   render() {
     if (__SERVER__) return '';
@@ -80,6 +124,7 @@ class Edit extends Component {
                     frames,
                   });
                 }}
+                chartHelp={chartHelp}
                 showFieldTooltips
                 useResizeHandler
                 debug
