0b66519484841e7d8f1aa69f833da47902563ede
ALEXANDRU MEDESAN <alex.medesan@gmail.com>
2023-09-28 16:39:14 +0300

    chore: clean code for sonar

--- src/ConnectedChart/ConnectedChart.jsx
+++ src/ConnectedChart/ConnectedChart.jsx
@@ -1,14 +1,12 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { compose } from 'redux';
 import cx from 'classnames';
-import loadable from '@loadable/component';
 import config from '@plone/volto/registry';
 import { toPublicURL } from '@plone/volto/helpers';
 import { connectToProviderData } from '@eeacms/volto-datablocks/hocs';
 import { updateChartDataFromProvider } from '@eeacms/volto-datablocks/helpers';
 import { connectBlockToVisualization } from '@eeacms/volto-plotlycharts/hocs';
 import { useHistory } from 'react-router-dom';
-
 import {
   Download,
   Sources,
@@ -16,12 +14,8 @@ import {
   MoreInfo,
   Enlarge,
 } from '@eeacms/volto-plotlycharts/Utils';
+import PlotlyComponent from './PlotlyComponent';
 
-const LoadablePlotly = loadable(() => import('react-plotly.js'));
-
-/*
- * ConnectedChart
- */
 function ConnectedChart(props) {
   const [firstLoad, setFirstLoad] = useState(true);
   const chartRef = useRef(null);
@@ -35,7 +29,7 @@ function ConnectedChart(props) {
     visualization,
     visualization_data,
   } = props;
-  const { hover_format_xy } = props.data || {};
+
   const {
     data_provenance,
     figure_note,
@@ -44,11 +38,16 @@ function ConnectedChart(props) {
     publisher,
     geo_coverage,
   } = visualization_data || {};
-  const use_live_data = props.data?.use_live_data ?? true;
-  const with_sources = props.data?.with_sources ?? false;
-  const with_notes = props.data?.with_notes ?? false;
-  const download_button = props.data?.download_button;
-  const with_more_info = props.data?.with_more_info ?? false;
+
+  const {
+    hover_format_xy,
+    use_live_data = true,
+    with_sources = false,
+    with_notes = false,
+    download_button,
+    with_more_info = false,
+  } = props.data || {};
+
   const loadingProviderData =
     loadingVisualizationData || (hasProviderUrl && props.loadingProviderData);
 
@@ -63,10 +62,10 @@ function ConnectedChart(props) {
       family: config.settings.chartLayoutFontFamily || "'Roboto', sans-serif",
     },
     margin: {
-      l: 80, // default: 80
-      r: 80, // default: 80
-      b: 80, // default: 80
-      t: 100, // default: 100
+      l: 80,
+      r: 80,
+      b: 80,
+      t: 100,
       ...(chartData.layout?.margin || {}),
     },
   };
@@ -118,98 +117,28 @@ function ConnectedChart(props) {
     return <div>Loading chart...</div>;
   }
 
-  return !Object.keys(chartData).length ? (
-    <div>No valid data.</div>
-  ) : (
+  if (!Object.keys(chartData).length) {
+    return <div>No valid data.</div>;
+  }
+
+  return (
     <div className="visualization-wrapper">
       <div className={cx('visualization', { autosize: layout.autosize })}>
-        <Enlarge data={data} layout={layout} history={history} />
-        <LoadablePlotly
-          onInitialized={(_, chartEl) => {
-            chartRef.current = chartEl;
-          }}
-          useResizeHandler
-          data={data}
-          layout={layout}
-          frames={[]}
-          config={{
-            displayModeBar: false,
-            editable: false,
-            responsive: true,
-          }}
-          onClick={(trace) => {
-            const { customLink, clickmode, meta = [] } = layout;
-            // Ex: catalogue?size=n_10_n&filters[0][field]={parent}&filters[0][values][0]={value}&filters[0][type]=any
-            // will not redirect on clicking a parent of point (treemap has a zoom feature and usually parents don't have
-            // the same significance as children, with relation to filter types)
-            if (customLink && clickmode !== 'none') {
-              const {
-                id,
-                label,
-                parent,
-                data = {},
-                curveNumber = 0,
-                pointIndex = 0,
-              } = trace?.points[0] || {};
-              const { type, parents = [], y = [], x = [] } = data;
-              const shouldRedirect = type
-                ? type !== 'treemap'
-                  ? true
-                  : parents.indexOf(id) === -1
-                  ? true
-                  : false
-                : false;
-              const shouldComposeLinks = meta.length > 0;
-
-              if (type === 'bar' && shouldComposeLinks) {
-                if (customLink === 'allLinks') {
-                  const yIsLabels = y.indexOf(label) > -1;
-                  const labels = yIsLabels
-                    ? y.filter((label) => label === 0 || label)
-                    : x.filter((label) => label === 0 || label); //trimming
-                  const noOfLabels = labels.length;
-                  const correspondingLinkPosition =
-                    noOfLabels * curveNumber + pointIndex;
-                  const correspondingLink = meta[correspondingLinkPosition];
-
-                  history.push(correspondingLink);
-                } else if (customLink === 'fullLinks') {
-                  const correspondingLinkPosition = pointIndex;
-                  const correspondingLink = meta[correspondingLinkPosition];
-
-                  history.push(correspondingLink);
-                }
-              } else if (shouldRedirect) {
-                const link = layout.customLink
-                  .replace('{value}', id || label)
-                  .replace('{parent}', parent);
-                history.push(link);
-              }
-            }
-          }}
-          onHover={(e) => {
-            if (layout.customLink && layout.clickmode !== 'none') {
-              e.event.target.style.opacity = 0.8;
-              e.event.target.style.transition = 'opacity 0.1s ease-in-out';
-              e.event.target.style.cursor = 'pointer';
-            }
-          }}
-          onUnhover={(e) => {
-            if (layout.customLink && layout.clickmode !== 'none') {
-              e.event.target.style.opacity = 1;
-              e.event.target.style.cursor = 'default';
-            }
-          }}
-          style={{
-            position: 'relative',
-            display: 'block',
-            ...(!layout.height ? { minHeight: '450px' } : {}),
-          }}
-        />
+        <Enlarge>
+          {/* For Enlarge we overwrite the custom size setting in order to enlarge it */}
+          <PlotlyComponent
+            {...{
+              chartRef,
+              data,
+              layout: { ...layout, autosize: true, height: null, width: null },
+              history,
+            }}
+          />
+        </Enlarge>
+        <PlotlyComponent {...{ chartRef, data, layout, history }} />
       </div>
       <div className="visualization-info-container">
         <div className="visualization-info">
-          {/* // behavior EEA Core metadata */}
           {with_notes && <FigureNote notes={figure_note} />}
           {with_sources && (
             <Sources
--- src/Utils/Enlarge/Enlarge.jsx
+++ src/Utils/Enlarge/Enlarge.jsx
@@ -1,12 +1,9 @@
 import React, { useState } from 'react';
 import { Modal, Button } from 'semantic-ui-react';
-import loadable from '@loadable/component';
 import fullscreenIcon from '@plone/volto/icons/fullscreen.svg';
 import { Icon } from '@plone/volto/components';
 
-const LoadablePlotly = loadable(() => import('react-plotly.js'));
-
-const EnlargeWidget = ({ data, layout, history }) => {
+const EnlargeWidget = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
@@ -15,87 +12,7 @@ const EnlargeWidget = ({ data, layout, history }) => {
         <Icon name={fullscreenIcon} size="24px" />
       </button>
       <Modal open={isOpen} className="plotly-enlarge-modal">
-        <Modal.Content>
-          <LoadablePlotly
-            useResizeHandler
-            data={data}
-            layout={{ ...layout, autosize: true, height: null, width: null }}
-            frames={[]}
-            config={{
-              displayModeBar: false,
-              editable: false,
-              responsive: true,
-            }}
-            onClick={(trace) => {
-              const { customLink, clickmode, meta = [] } = layout;
-              // Ex: catalogue?size=n_10_n&filters[0][field]={parent}&filters[0][values][0]={value}&filters[0][type]=any
-              // will not redirect on clicking a parent of point (treemap has a zoom feature and usually parents don't have
-              // the same significance as children, with relation to filter types)
-              if (customLink && clickmode !== 'none') {
-                const {
-                  id,
-                  label,
-                  parent,
-                  data = {},
-                  curveNumber = 0,
-                  pointIndex = 0,
-                } = trace?.points[0] || {};
-                const { type, parents = [], y = [], x = [] } = data;
-                const shouldRedirect = type
-                  ? type !== 'treemap'
-                    ? true
-                    : parents.indexOf(id) === -1
-                    ? true
-                    : false
-                  : false;
-                const shouldComposeLinks = meta.length > 0;
-
-                if (type === 'bar' && shouldComposeLinks) {
-                  if (customLink === 'allLinks') {
-                    const yIsLabels = y.indexOf(label) > -1;
-                    const labels = yIsLabels
-                      ? y.filter((label) => label === 0 || label)
-                      : x.filter((label) => label === 0 || label); //trimming
-                    const noOfLabels = labels.length;
-                    const correspondingLinkPosition =
-                      noOfLabels * curveNumber + pointIndex;
-                    const correspondingLink = meta[correspondingLinkPosition];
-
-                    history.push(correspondingLink);
-                  } else if (customLink === 'fullLinks') {
-                    const correspondingLinkPosition = pointIndex;
-                    const correspondingLink = meta[correspondingLinkPosition];
-
-                    history.push(correspondingLink);
-                  }
-                } else if (shouldRedirect) {
-                  const link = layout.customLink
-                    .replace('{value}', id || label)
-                    .replace('{parent}', parent);
-                  history.push(link);
-                }
-              }
-            }}
-            onHover={(e) => {
-              if (layout.customLink && layout.clickmode !== 'none') {
-                e.event.target.style.opacity = 0.8;
-                e.event.target.style.transition = 'opacity 0.1s ease-in-out';
-                e.event.target.style.cursor = 'pointer';
-              }
-            }}
-            onUnhover={(e) => {
-              if (layout.customLink && layout.clickmode !== 'none') {
-                e.event.target.style.opacity = 1;
-                e.event.target.style.cursor = 'default';
-              }
-            }}
-            style={{
-              position: 'relative',
-              display: 'block',
-              ...(!layout.height ? { minHeight: '450px' } : {}),
-            }}
-          />
-        </Modal.Content>
+        <Modal.Content>{children}</Modal.Content>
         <Modal.Actions>
           {' '}
           <Button onClick={() => setIsOpen(false)}>Close</Button>
