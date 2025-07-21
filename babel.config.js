4f8b2ee3712bec09f94d98fc4b9bd93f966ec935
dobri1408 <50819975+dobri1408@users.noreply.github.com>
2025-07-21 12:31:20 +0300

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,77 +1,17 @@
-const path = require('path');
-
 module.exports = function (api) {
   api.cache(true);
   const presets = ['razzle'];
   const plugins = [
     [
-      'react-intl',
+      'react-intl', // React Intl extractor, required for the whole i18n infrastructure to work
       {
         messagesDir: './build/messages/',
       },
     ],
-    [
-      'transform-imports',
-      {
-        '@plone/volto/helpers': {
-          transform: (importName) =>
-            `@plone/volto/helpers/${importName}`,
-          preventFullImport: true,
-        },
-        'date-fns': {
-          transform: (importName) => `date-fns/${importName}`,
-          preventFullImport: true,
-        },
-        lodash: {
-          transform: (importName) => `lodash/${importName}`,
-          preventFullImport: true,
-        },
-      },
-    ],
-    'transform-class-properties',
-    [
-      'babel-plugin-root-import',
-      {
-        rootPathSuffix: 'src',
-      },
-    ],
   ];
-  const env = {
-    test: {
-      plugins: [
-        'istanbul',
-        [
-          'babel-plugin-root-import',
-          {
-            rootPathSuffix: 'src',
-          },
-        ],
-      ],
-    },
-  };
-
-  if (process.env.NODE_ENV !== 'development') {
-    plugins.push([
-      'transform-imports',
-      {
-        '../../theme.config
-: {
-          transform: (importName) => {
-            const p = path.resolve(
-              `./theme/themes/eea/theme.config`,
-            );
-            return p;
-          },
-          preventFullImport: true,
-        },
-      },
-      'eea-volto-plotlycharts-theme-config',
-    ]);
-  }
 
   return {
-    presets,
     plugins,
-    env,
+    presets,
   };
 };
